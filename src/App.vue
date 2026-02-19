49ddcdd2e25e730068b7c56e44dfad9c32bd30cb
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-02-19 13:14:31 +0800

    element主题色的定制

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,11 @@
-<script setup></script>
+<script setup>
+</script>
 
 <template>
+  <!-- Element Plus 是一套 基于 Vue 3 的 UI 组件库，
+   提供了大量可复用的组件（Button、Table、Form、Dialog、DatePicker 等）。
+   它本质上是 封装好的 Vue 组件，内置了样式、交互逻辑和一些最佳实践。 -->
+    <el-button type="primary">Primary</el-button>
   <h1>You did it!</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
