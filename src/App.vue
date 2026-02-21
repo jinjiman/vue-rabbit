768c36883a761993e09743d2e22f1391a63bd13d
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-02-21 16:40:18 +0800

    layout静态框架的搭建

--- src/App.vue
+++ src/App.vue
@@ -7,23 +7,11 @@ import { RouterView } from 'vue-router';
   <!-- Element Plus 是一套 基于 Vue 3 的 UI 组件库，
    提供了大量可复用的组件（Button、Table、Form、Dialog、DatePicker 等）。
    它本质上是 封装好的 Vue 组件，内置了样式、交互逻辑和一些最佳实践。 -->
-    <el-button type="primary">Primary</el-button>
   
     <!-- 写一个一级路由出口组件 -->
   <router-view/>
-  <div class="test">
-    test scss
-  </div>
-
-  <!-- <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p> -->
 </template>
 
 <style scoped lang="scss">
-  .test{
-    color: $priceColor;
-  }
+
 </style>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -1,4 +1,12 @@
+<script setup>
+import LayoutNav from '@/views/layout/compenents/LayoutNav.vue'
+import LayoutHeader from '@/views/layout/compenents/LayoutHeader.vue'
+import LayoutFooter from '@/views/layout/compenents/LayoutFooter.vue'
+</script>
+
 <template>
-  <div>我是首页</div>
-  <RouterView/>
+  <LayoutNav />
+  <LayoutHeader />
+  <RouterView />
+  <LayoutFooter />
 </template>
