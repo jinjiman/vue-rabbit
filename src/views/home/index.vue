56a0f3fe2ef977e294de22fcf82d0200ab8c9fd4
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-02-26 20:05:26 +0800

    吸顶导航的交互实现

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,3 @@
 <template>
-  <div>我是home页面</div>
+  <div style="height: 1200PX;">我是home页面</div>
 </template>
--- src/views/layout/index.vue
+++ src/views/layout/index.vue
@@ -2,9 +2,11 @@
 import LayoutNav from '@/views/layout/compenents/LayoutNav.vue'
 import LayoutHeader from '@/views/layout/compenents/LayoutHeader.vue'
 import LayoutFooter from '@/views/layout/compenents/LayoutFooter.vue'
+import LayoutFixed from '@/views/layout/compenents/LayoutFixed.vue';
 </script>
 
 <template>
+  <LayoutFixed/>
   <LayoutNav />
   <LayoutHeader />
   <RouterView />
