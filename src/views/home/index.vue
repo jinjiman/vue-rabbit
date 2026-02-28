4ff75e8e3fd0bdd8c4834657617af36d97f53586
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-02-28 18:31:35 +0800

    Home整体结构的搭建

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,3 +1,17 @@
+<script setup>
+import HomeCategory from '@/views/home/components/HomeCategory.vue'
+import HomeBanner from '@/views/home/components/HomeBanner.vue'
+import HomeNew from '@/views/home/components/HomeNew.vue'
+import HomeHot from '@/views/home/components/HomeHot.vue'
+import homeProduct from '@/views/home/components/HomeProduct.vue'
+</script>
+
 <template>
-  <div style="height: 1200PX;">我是home页面</div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <homeProduct />
 </template>
