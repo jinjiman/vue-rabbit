0b97c275f82a710fc0640f8327ac21dce90a524a
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-03-01 01:16:58 +0800

    面板组件的封装

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -3,7 +3,8 @@ import HomeCategory from '@/views/home/components/HomeCategory.vue'
 import HomeBanner from '@/views/home/components/HomeBanner.vue'
 import HomeNew from '@/views/home/components/HomeNew.vue'
 import HomeHot from '@/views/home/components/HomeHot.vue'
-import homeProduct from '@/views/home/components/HomeProduct.vue'
+import HomeProduct from '@/views/home/components/HomeProduct.vue'
+import HomePanel from '@/views/home/components/HomePanel.vue'
 </script>
 
 <template>
@@ -13,5 +14,13 @@ import homeProduct from '@/views/home/components/HomeProduct.vue'
   </div>
   <HomeNew />
   <HomeHot />
-  <homeProduct />
+  <HomeProduct />
+  <!-- 在 Vue 3 模板中传递 props 时，驼峰命名必须改成短横线形式（kebab-case），不能直接写驼峰。 -->
+  <HomePanel title="新鲜好物" sub-title="新鲜好物,好多物品">
+    <div>111</div>
+  </HomePanel>
+  
+  <HomePanel title="人气推荐" sub-title="人气推荐,好多物品">
+    <div>222</div>
+  </HomePanel>
 </template>
