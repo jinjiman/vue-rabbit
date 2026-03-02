a5afcd887367d342851c6ad1541b77dcf4e2b743
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-03-02 10:22:56 +0800

    懒加载的实现

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import App from './App.vue'
 import router from './router'
 //引用初始化样式文件：
 import '@/styles/common.scss'
+import { useIntersectionObserver } from '@vueuse/core'
 //测试接口函数
 // import { getCategory } from '@/apis/testApi'
 // getCategory().then(res => {
@@ -15,4 +16,21 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 
+//app的directive方法可以自定义指令
+
 app.mount('#app')
+
+app.directive('img-lazy', {
+  mounted(el, binding) {
+    // el:指定绑定的那个元素
+    // binding:指定指令的“=”右边的值，这里指的是图片URL 
+    useIntersectionObserver(
+      el, ([{ isIntersecting }]) => {
+        if (isIntersecting) {
+          // 进入视口区域
+          el.src = binding.value
+        }
+      }
+    )
+  }
+})
