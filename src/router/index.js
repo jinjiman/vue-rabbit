bb22cafc41b47fb5ed14c8ed601553117c4fb09a
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-03-09 09:01:35 +0800

    详情页整体认识和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import Layout from '@/views/layout/index.vue'
 import Home from '@/views/home/index.vue'
 import Category from '@/views/category/index.vue'
 import SubCategory from '@/views/SubCategory/index.vue'
+import Detail from '@/views/Detail/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -24,6 +25,10 @@ const router = createRouter({
         {
           path: 'category/sub/:id',
           component: SubCategory
+        },
+        {
+          path: 'detail/:id',
+          component: Detail
         }
       ]
     },
