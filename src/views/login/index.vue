8b6712b1545db7429c2bc4835042528596058739
Boro_Gati <Boro_Gati@noreply.gitcode.com>
2026-03-11 18:58:10 +0800

    自定义校验功能

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -4,7 +4,8 @@ import {ref} from 'vue'
 // 1.准备表单对象
 const form = ref({
   account:'',
-  password:''
+  password:'',
+  agree:true
 })
 // 2.准备规则对象
 const rules = {
@@ -14,6 +15,17 @@ const rules = {
   password:[
     { required:true, message: '密码不能为空',trigger:'blur' },
     { min: 6, max: 14, message:'密码为6-14个字符',trigger:'blur'}
+  ],
+  agree:[
+    {
+      validator:(rule, val, callback) => {
+      console.log(val);
+      if(val){
+        callback()
+      }else{
+        callback(new Error('请勾选协议'))
+      }
+    }}
   ]
 }
 </script>
@@ -47,8 +59,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password"/>
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox  size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox  v-model="form.agree" size="large">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
